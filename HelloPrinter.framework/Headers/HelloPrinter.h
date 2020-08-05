7b8136291dcf8ad2f05b69c37466681f4d9861a4
Matthew O'Leary <moleary@zendesk.com>
2020-08-05 01:44:25 +0100

    Adding HelloPrinter

---
+++ HelloPrinter.framework/Headers/HelloPrinter.h
@@ -0,0 +1,19 @@
+//
+//  HelloPrinter.h
+//  HelloPrinter
+//
+//  Created by Matthew O'Leary on 05/08/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloPrinter.
+FOUNDATION_EXPORT double HelloPrinterVersionNumber;
+
+//! Project version string for HelloPrinter.
+FOUNDATION_EXPORT const unsigned char HelloPrinterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloPrinter/PublicHeader.h>
+
+
