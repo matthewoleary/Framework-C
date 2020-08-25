0ef01ba8c8c98eaf1f55aa8ec885032a687e546f
zendesk-moleary <moleary@zendesk.com>
2020-08-25 14:54:40 +0100

    Adding framework source code

---
+++ HelloPrinter/HelloPrinter/HelloPrinter.swift
@@ -0,0 +1,15 @@
+//
+//  HelloPrinter.swift
+//  HelloPrinter
+//
+//  Created by Matthew O'Leary on 24/08/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+public class HelloPrinter {
+    public init () {}
+    
+    public func hello() {
+        print("Hello from Framework-C")
+    }
+}
